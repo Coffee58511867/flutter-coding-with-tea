e456e057299ff45907244598819b05826618464a
Coffee58511867 <marobahlalele910@gmail.com>
2023-07-01 11:23:29 +0200

    Added Constants of app

--- lib/src/features/authentication/screens/homepage/home_page.dart
+++ lib/src/features/authentication/screens/homepage/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:coding_with_tea/src/constants/size.dart';
 import 'package:flutter/material.dart';
 
 import '../../../../constants/image_string.dart';
@@ -15,14 +16,47 @@ class _HomePage1State extends State<HomePage1> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        child: Column(children: [
-          Image(image: AssetImage(tWelcomeScreen)),
-          Text(tWelcomeTitle),
-          Text(tWelcomeSubtitle),
+        padding: EdgeInsets.all(tDefaultSize),
+        child:
+            Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+          const Image(image: AssetImage(tWelcomeScreen)),
+          Column(
+            children: [
+              Text(tWelcomeTitle,
+                  style: Theme.of(context).textTheme.displayMedium),
+              Text(
+                tWelcomeSubtitle,
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
           Row(
             children: [
-              OutlinedButton(onPressed: () {}, child: const Text(tLogin)),
-              ElevatedButton(onPressed: () {}, child: const Text(tRegister)),
+              Expanded(
+                child: OutlinedButton(
+                    onPressed: () {},
+                    style: OutlinedButton.styleFrom(
+                      shape: RoundedRectangleBorder(),
+                      foregroundColor: Colors.amber,
+                      side: BorderSide(color: Colors.black12),
+                      padding: EdgeInsets.symmetric(vertical: tButtonHeight),
+                    ),
+                    child: Text(tLogin.toUpperCase())),
+              ),
+              const SizedBox(
+                width: 10,
+              ),
+              Expanded(
+                child: ElevatedButton(
+                    onPressed: () {},
+                    style: OutlinedButton.styleFrom(
+                      shape: RoundedRectangleBorder(),
+                      foregroundColor: Colors.amber,
+                      side: BorderSide(color: Colors.black12),
+                      padding: EdgeInsets.symmetric(vertical: tButtonHeight),
+                    ),
+                    child: Text(tRegister.toUpperCase())),
+              )
             ],
           )
         ]),
--- lib/src/utils/theme/widgets_theme/text_theme.dart
+++ lib/src/utils/theme/widgets_theme/text_theme.dart
@@ -5,6 +5,7 @@ class TTextTheme {
   static TextTheme lightTextTheme = TextTheme(
     displayMedium: GoogleFonts.montserrat(color: Colors.blue, fontSize: 30),
     displaySmall: GoogleFonts.montserrat(color: Colors.black12, fontSize: 18),
+    bodyMedium: GoogleFonts.poppins(fontSize: 15, color: Colors.black12),
   );
 
   static TextTheme darkTextTheme = TextTheme();
