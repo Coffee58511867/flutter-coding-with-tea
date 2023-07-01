fd242ada5d79fe4553a0368fc40a5d60e4f49fd1
Coffee58511867 <marobahlalele910@gmail.com>
2023-07-01 10:39:37 +0200

    Added Constants of app

---
+++ lib/src/constants/text_strings.dart
@@ -0,0 +1,8 @@
+// APP TEXT STRINGS
+
+const String tAppName = "Notifications";
+
+// -- Welcome Screen text
+
+const String tWelcomeTitle = "Build Awesome Apps";
+const String tWelcomeSubtitle = "Lets put your creativity on developmet";
